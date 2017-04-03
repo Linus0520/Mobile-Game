93718b6ece72d18a93f2621aaa2c47cb4dec3c3c
Linus <cupzhen@gmail.com>
2017-04-03 13:53:57 -0400

    load
    load

---
+++ www/js/controller.js
@@ -0,0 +1,31 @@
+var app = function(app) {
+
+    app.makeController = function(stage, p, pages) {
+
+
+     //    pages.on("page",function(e){
+    	// 	zog(pages.page.name)
+    	// });
+
+    	pages.on("pagetransitioned",function(e){
+    		zog(pages.page.name)
+    	});
+
+
+    	var hotSpots = new zim.HotSpots([
+    	    {page:p.page1, rect:p.page1.tabs.buttons[1], call:function(){pages.go(p.page2, 'down');}},
+		    {page:p.page2, rect:p.page2.logo, call:function(){pages.go(p.page1, 'up');}}
+		]);
+
+        // p.page1.tabs.on("change", function() {
+        //     zog("button = " + p.page1.tabs.text);
+        //     pages.go(p.page2, 'down'); //direction you are going to
+        // });
+
+        // p.page2.logo.on("mousedown",function(){
+        // 	pages.go(p.page1, 'up');
+        // })
+
+    }
+    return app;
+}(app || {});
